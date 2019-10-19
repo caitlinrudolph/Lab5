3505615db432bbf1ef7848b81767f175301da528
caitlinrudolph <brooke_rudolph@hotmail.com>
2019-10-19 10:53:16 -0500

    setting up base for execution

--- src/FibLoop.java
+++ src/FibLoop.java
@@ -1,3 +1,74 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+import java.util.Arrays;
+
 public class FibLoop {
     //a dynamic programming version using a straight forward loop to add up numbers in the sequence and return the nth number
+    static ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+
+    /* define constants */
+    static int numberOfTrials = 128;
+
+    static String ResultsFolderPath = "/home/caitlin/Documents/Lab5/"; // pathname to results folder
+    static FileWriter resultsFile;
+    static PrintWriter resultsWriter;
+
+    public static void main(String[] args) {
+
+        //direct the verification test results to file
+        // run the whole experiment at least twice, and expect to throw away the data from the earlier runs, before java has fully optimized
+        System.out.println("Running first full experiment...");
+        runFullExperiment("FibLoop-Exp1-ThrowAway.txt");
+        System.out.println("Running second full experiment...");
+        runFullExperiment("FibLoop-Exp2.txt");
+        System.out.println("Running third full experiment...");
+        runFullExperiment("FibLoop-Exp3.txt");
+
+    }
+
+    static void runFullExperiment(String resultsFileName) {
+        try {
+            resultsFile = new FileWriter(ResultsFolderPath + resultsFileName);
+            resultsWriter = new PrintWriter(resultsFile);
+        } catch (Exception e) {
+            System.out.println("*****!!!!!  Had a problem opening the results file " + ResultsFolderPath + resultsFileName);
+            return; // not very foolproof... but we do expect to be able to create/open the file...
+        }
+
+        ThreadCpuStopWatch TrialStopwatch = new ThreadCpuStopWatch(); // for timing an individual trial
+
+        resultsWriter.println("#X(value)    N(size)     T(time)"); // # marks a comment in gnuplot data
+        resultsWriter.flush();
+        /* for each size of input we want to test: in this case starting small and doubling the size each time */
+        for (int  i = 0;  i <= numberOfTrials; i ++) {
+            //for each integer <= 128 run a trial
+            // progress message...
+            System.out.println("Running test for input size " + i + " ... ");
+
+            /* repeat for desired number of trials (for a specific size of input)... */
+            long batchElapsedTime = 0;
+
+            System.out.print("    Running trial batch...");
+
+            /* force garbage collection before each batch of trials run so it is not included in the time */
+            System.gc();
+
+            // run the trials
+
+            TrialStopwatch.start(); // *** uncomment this line if timing trials individually
+
+            batchElapsedTime = batchElapsedTime + TrialStopwatch.elapsedTime(); // *** uncomment this line if timing trials individually
+
+
+            //batchElapsedTime = BatchStopwatch.elapsedTime(); // *** comment this line if timing trials individually
+            double averageTimePerTrialInBatch = (double) batchElapsedTime / (double) numberOfTrials; // calculate the average time per trial in this batch
+
+            /* print data for this size of input */
+            resultsWriter.printf("%12d  %12d  %12d \n", value, size, averageTimePerTrialInBatch); // might as well make the columns look nice
+            resultsWriter.flush();
+            System.out.println(" ....done.");
+        }
+    }
 }
